bafa8cff62c6e9b9513d1995eb5a88a256ea271b
lilium724 <louregnier724@gmail.com>
2025-04-01 14:54:27 +0200

    started eye option parsing

--- src/parser.c
+++ src/parser.c
@@ -1,13 +1,19 @@
 #include <string.h>
+#include <stdio.h>
 
 int parse_arguments(int argc, char **argv, char *text, char *eyes) {
   int parse_text = 0;
   for(int i = 1; i < argc; i++) {
     if(!parse_text) {
-      if(strncmp(argv[i], "-", 1))
+      if(argv[i][0] != '-')
         parse_text = 1;
-      else
+      else {
+        if(argv[i][1] == 'e' || !strcmp(argv[i], "--eyes")) {
+          //TODO: eyes option parser
+        }
+
         i++;
+      }
     }
     if(parse_text) {
       strcat(text, argv[i]);
