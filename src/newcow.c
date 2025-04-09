26f69c6247175f9d0f5ea4c2162545a24717f5f0
Jkmi <gabwill.anne.jeux@gmail.com>
2025-04-10 00:19:26 +0200

    le texte est maintenant dans une bulle #2

--- src/newcow.c
+++ src/newcow.c
@@ -14,13 +14,26 @@ void affiche_vache(char *eyes, char *legs) {
  %s\n ", eyes, legs);
 }
 
+/*fonction qui crée et affiche la bulle de texte*/
+void bulle (char *text){
+	char top[100]="";
+	char bot[100]="";
+	/*passe sur chaque caractère de la liste et rajoute les contours*/
+	for (int i = 0; strcmp(&text[i],"\0"); i++){
+		strcat(top, "_");
+		strcat(bot,"-");
+	}
+	/*affiche le texte avec le bon format*/
+	printf(" %s\n<%s>\n %s\n", top, text, bot);
+}
+
 int main(int argc, char **argv) {
 	char text[100] = "";
   	char eyes[3] = "00";
 	char legs[100] = "";
 	int ret_val = parse_arguments(argc, argv, text, eyes, legs);
 	if (ret_val == 0){
-		printf("%s\n", text);
+		bulle(text); /*affiche la bulle avec le texte*/
 		affiche_vache(eyes, legs);
 	}
 	return ret_val;
