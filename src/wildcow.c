38f36923c42c76c8e566548ddc344f7a64711d5e
Jkmi <gabwill.anne.jeux@gmail.com>
2025-04-11 17:00:26 +0200

    teste fonction goto

--- src/wildcow.c
+++ src/wildcow.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
-/*Teste push*/
 
 void update () { 
     printf ( "\033[H\033[J" ); 
@@ -13,7 +11,7 @@ void gotoxy ( int x , int y ) {
 }
 
 int main (){
-    gotoxy(100, 100);
+    gotoxy(100, 50);
     printf("%s\n%s\n", "hello", "world");
     return 0;
 }
