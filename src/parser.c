78e8736e40c94b31fac149ce85aed303abcd3754
lilium724 <louregnier724@gmail.com>
2025-04-13 15:16:18 +0200

    reformat parser.c

--- src/parser.c
+++ src/parser.c
@@ -3,66 +3,69 @@
 #include <stdlib.h> /*ajout de stdlib pour utiliser atoi*/
 
 int parse_arguments(int argc, char **argv, char *text, char eyes[static 3], int *legs, char *tongue, char *queue) {
-  int parse_text = 0;
-  for(int i = 1; i < argc; i++) {
-    if(!parse_text) {
-      if(argv[i][0] != '-'){
-        parse_text = 1;
+  int i;
+  for(i = 1; i < argc; i++) {
+    if(argv[i][0] != '-'){
+      break;
+    }
+
+    /*Option pour les yeux */
+    if(argv[i][1] == 'e' || !strcmp(argv[i], "--eyes")) {
+      if(i == argc - 1) {
+        fprintf(stderr, "L'option %s attend un argument!\n", argv[i]);
+        return 1;
       }
-      else {
-        if(argv[i][1] == 'e' || !strcmp(argv[i], "--eyes")) {
-          if(i == argc - 1) {
-            fprintf(stderr, "L'option %s attend un argument!\n", argv[i]);
-            return 1;
-          }
           
-          strncpy(eyes, argv[i+1], 2);
-          eyes[3] = 0;
+      strncpy(eyes, argv[i+1], 2);
+      eyes[3] = 0;
+      i++;
+    }
 
-          i++;
-        }
-        /*Option pour "Makes the cow appear thoroughly stoned."*/
-        else if(argv[i][1]=='s'){ 
-          strcpy(eyes, "**"); /*change les yeux de la vache*/
-          strcpy(tongue,"U ");/*change la langue*/
-        }
-        /*action qui rallonge les pied de la vache*/
-        else if (argv[i][1] == 't' || !strcmp(argv[i], "--tall")){ 
-          if(i == argc - 1) {
-            fprintf(stderr, "L'option %s attend un argument!\n", argv[i]);
-            return 1;
-          }
-         *legs = atoi(argv[i+1]); 
-          i++;
-        }
-        else if (argv[i][1] == 'q'){
-          if(i == argc - 1) {
-            fprintf(stderr, "L'option %s attend un argument!\n", argv[i]);
-            return 1;
-          }
+    /*Option pour "Makes the cow appear thoroughly stoned."*/
+    else if(argv[i][1]=='s'){ 
+      strcpy(eyes, "**"); /*change les yeux de la vache*/
+       strcpy(tongue,"U ");/*change la langue*/
+    }
 
-          for (int y = 0; y<atoi(argv[i+1]); y++){
-            if (y%2 == 1){
-              strcat(queue,"\\");
-            }
-            else{
-              strcat(queue, "/");
-            }
-          }
-          i++;
-        }
-        else {
-          fprintf(stderr, "L'argument %s n'existe pas!", argv[i]);
-          return 2;
-        }
-
+    /*Option qui rallonge les pied de la vache*/
+    else if (argv[i][1] == 't' || !strcmp(argv[i], "--tall")){ 
+      if(i == argc - 1) {
+        fprintf(stderr, "L'option %s attend un argument!\n", argv[i]);
+        return 1;
       }
+      *legs = atoi(argv[i+1]); 
+      i++;
     }
-    if(parse_text) {
-      strcat(text, argv[i]);
-      if(i < argc - 1)
-        strcat(text, " ");
+
+    /*Option qui ralonge la queue de la vache*/
+    else if (argv[i][1] == 'q'){
+      if(i == argc - 1) {
+        fprintf(stderr, "L'option %s attend un argument!\n", argv[i]);
+        return 1;
+      }
+
+      for (int y = 0; y<atoi(argv[i+1]); y++){
+        if (y%2 == 1){
+          strcat(queue,"\\");
+        }
+        else{
+          strcat(queue, "/");
+        }
+      }
+      i++;
     }
+
+    else {
+      fprintf(stderr, "L'argument %s n'existe pas!", argv[i]);
+      return 2;
+    }
+  }
+
+  /*Le reste des arguments est traité comme du texte*/
+  for(;i < argc; i++) {
+    strcat(text, argv[i]);
+    if(i < argc - 1)
+      strcat(text, " ");
   }
   return 0;  
 }
