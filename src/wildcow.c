7088584b22e6b2bfc58a3be34dad62ec9ae9128c
Jkmi <gabwill.anne.jeux@gmail.com>
2025-04-14 14:51:20 +0200

    teste animation

--- src/wildcow.c
+++ src/wildcow.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "newcow.h"
 void update () { 
     printf ( "\033[H\033[J" ); 
 }
@@ -11,10 +10,28 @@ void gotoxy ( int x , int y ) {
     printf ( "\033[%d;%dH" ,x , y ); 
 }
 
+char *noyade[] = {
+"   (__) \n\
+        (oo)\n\
+ /-------\\/\n\
+/ |     ||\n\
+*  ||----||\n\
+  ~~    ~~"
+};
+
+void affiche_vache(char **anim, int x, int y){
+    for (int i = 0; &anim[i]; i++){
+        gotoxy(x,y);
+        for (int j = 0; &anim[i][j]; j++){
+            printf("%c", anim[i][j]);
+        }
+    }
+}
+
 int main (){
     for (int i = 0; i<10 ; i++){
         gotoxy(i,i*2);
-        printf("hello\n");
+        affiche_vache(noyade, 100, 100);
         usleep(100000);
         update();
     }
